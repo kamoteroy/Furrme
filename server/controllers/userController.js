f4d125f7b44f6b12e98cec57464a7eb5f39e02dd
kamoteroy <kamoteroy6@gmail.com>
2025-04-20 09:32:09 +0800

    update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const db = require("../mysql");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cloud = require("cloudinary").v2;
 
